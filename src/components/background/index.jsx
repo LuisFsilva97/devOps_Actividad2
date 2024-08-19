81c68a1752b1b92f6c3e63793b70eb66991ff96f
Luis Fernando Silva Betancurt <Darklegen97@gmail.com>
2024-08-18 21:58:15 -0500

    ver 1.1.1
    new text Header

--- src/components/background/index.jsx
+++ src/components/background/index.jsx
@@ -5,7 +5,8 @@ import { Container } from "./style"
 export const BackgroundSection = () => {
     return (
         <Container>
-            <h1>The Rick and Morty API</h1>
+            <h1>The Rick and Morty API</h1> <br></br>
+            <h5>By: Luis Fernando Silva Betancurt</h5>
         </Container>
     )
 
